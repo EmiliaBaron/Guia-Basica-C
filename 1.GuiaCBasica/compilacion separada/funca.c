385a89c4c5d44ec4834fdbb0cacac129e0082c51
Emilia Sofía Baron <emiliasbaron@gmail.com>
2025-09-02 11:08:43 -0300

    termine la guia el martes 2/9

---
+++ 1.GuiaCBasica/compilacion separada/funca.c
@@ -0,0 +1,8 @@
+/* funca.c */
+#include "funca.h"
+#include <stdio.h>
+
+void a()
+{
+    printf("Hola, soy A!\n");
+}
---
+++ 1.GuiaCBasica/compilacion separada/main.c
@@ -0,0 +1,9 @@
+/* main.c */
+#include "funca.h"
+#include "funcb.h"
+
+int main()
+{
+    a();
+    b();
+}
